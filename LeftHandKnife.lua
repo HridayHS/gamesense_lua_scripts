dedc67a98b5f89912bec061f5c63919b4bdaa576
HridayHS <hridaysharma42@gmail.com>
2020-05-20 21:53:55 +0530

    Add left hand knife lua

---
+++ LeftHandKnife.lua
@@ -0,0 +1,18 @@
+local LeftHandKnife = {
+	Enabled = ui.new_checkbox('Lua', 'B', 'Left Hand Knife'),
+	Main = function()
+		if entity.get_classname(entity.get_player_weapon(entity.get_local_player())) == 'CKnife' then
+			client.exec('cl_righthand 0')
+		else
+			client.exec('cl_righthand 1')
+		end
+	end	
+}
+
+ui.set_callback(LeftHandKnife.Enabled, function(itemNumber)
+	if ui.get(itemNumber) then
+		client.set_event_callback('setup_command', LeftHandKnife.Main)
+	else
+		client.unset_event_callback('setup_command', LeftHandKnife.Main)
+	end
+end)
