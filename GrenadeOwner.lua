4fe0701bd356d3f3e60c2db7651908705117d158
Hriday Sharma <hridaysharma42@gmail.com>
2020-05-24 02:50:50 +0530

    GrenadeOwner: Improvements
    * Hide grenades multiselect and enemy name checkbox if grenade owner is disabled.
    * Code Improvements
    * Visual Improvements

--- GrenadeOwner.lua
+++ GrenadeOwner.lua
@@ -1,13 +1,19 @@
+local References = {
+	GrenadeESP = ui.reference('VISUALS', 'Other ESP', 'Grenades')
+}
+
 local GrenadeOwner = {
 	Label = ui.new_label('Lua', 'A', 'Grenade Owner'),
 	Enabled = ui.new_checkbox('Lua', 'A', 'Enabled'),
 	Grenades = ui.new_multiselect('Lua', 'A', 'Grenades', 'Decoy', 'Smoke'),
 	EnemyName = ui.new_checkbox('Lua', 'A', 'Show enemy name'),
 }
-local Ref_GrenadeESP = ui.reference('VISUALS', 'Other ESP', 'Grenades')
+
+ui.set_visible(GrenadeOwner.Grenades, false)
+ui.set_visible(GrenadeOwner.EnemyName, false)
 
 GrenadeOwner.DrawOwnerName = function ()
-	local isGrenadeESPOn = ui.get(Ref_GrenadeESP)
+	local isGrenadeESPOn = ui.get(References.GrenadeESP)
 	local Grenades = ui.get(GrenadeOwner.Grenades)
 
 	for i=1, #Grenades do
@@ -23,7 +29,7 @@ GrenadeOwner.DrawOwnerName = function ()
 					local RenderTextString	
 
 					if DecoyOwnerEntity == entity.get_local_player() then
-						RenderTextString = 'OWN'
+						RenderTextString = 'YOUR'
 					elseif not entity.is_enemy(DecoyOwnerEntity) then
 						RenderTextString = 'TEAMMATE  ' .. DecoyOwnerName
 					elseif entity.is_enemy(DecoyOwnerEntity) and ui.get(GrenadeOwner.EnemyName) then
@@ -52,7 +58,7 @@ GrenadeOwner.DrawOwnerName = function ()
 					local RenderTextString
 
 					if SmokeOwnerEntity == entity.get_local_player() then
-						RenderTextString = 'OWN'
+						RenderTextString = 'YOUR'
 					elseif not entity.is_enemy(SmokeOwnerEntity) then
 						RenderTextString = 'TEAMMATE  ' .. SmokeOwnerName
 					elseif entity.is_enemy(SmokeOwnerEntity) and ui.get(GrenadeOwner.EnemyName) then
@@ -72,7 +78,14 @@ GrenadeOwner.DrawOwnerName = function ()
 end
 
 ui.set_callback(GrenadeOwner.Enabled, function (itemNumber)
-	if ui.get(itemNumber) then
+	local isGrenadeOwnerEnabled = ui.get(itemNumber)
+
+	-- Handle Menu Items
+	ui.set_visible(GrenadeOwner.Grenades, isGrenadeOwnerEnabled)
+	ui.set_visible(GrenadeOwner.EnemyName, isGrenadeOwnerEnabled)
+
+	-- Main
+	if isGrenadeOwnerEnabled then
 		client.set_event_callback('paint', GrenadeOwner.DrawOwnerName)
 	else
 		client.unset_event_callback('paint', GrenadeOwner.DrawOwnerName)
