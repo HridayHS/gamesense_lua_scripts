c3bece1ffbe7a0b0496ec4a4e4e900bead2f2ffc
Hriday Sharma <hridaysharma42@gmail.com>
2020-06-02 06:53:02 +0530

    LeftHandKnife: Add support for class CKnifeGG
    * CKnifeGG: Arms race knife

--- LeftHandKnife.lua
+++ LeftHandKnife.lua
@@ -1,7 +1,11 @@
 local entity_is_alive, entity_get_prop, entity_get_classname, entity_get_local_player, entity_get_player_weapon = entity.is_alive, entity.get_prop, entity.get_classname, entity.get_local_player, entity.get_player_weapon
 
+local function GetWeaponClassname()
+	return entity_get_classname(entity_get_player_weapon(entity_is_alive(entity_get_local_player()) and entity_get_local_player() or entity_get_prop(entity_get_local_player(), 'm_hObserverTarget')))
+end
+
 client.set_event_callback('pre_render', function ()
-	if entity_get_classname(entity_get_player_weapon(entity_is_alive(entity_get_local_player()) and entity_get_local_player() or entity_get_prop(entity_get_local_player(), 'm_hObserverTarget'))) == 'CKnife' then
+	if GetWeaponClassname() == 'CKnife' or GetWeaponClassname() == 'CKnifeGG' then
 		cvar.cl_righthand:set_int(0)
 	else
 		cvar.cl_righthand:set_int(1)
