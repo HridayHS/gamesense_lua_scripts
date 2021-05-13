e309e156712f491f2114e29f0430ce3db54dcd05
Hriday Sharma <hridaysharma42@gmail.com>
2021-05-13 16:55:42 +0530

    FakelagIndicator: Update
    * Add support for extending line width
    * Add color picker to line, fakelag value text and circle

--- FakelagIndicator.lua
+++ FakelagIndicator.lua
@@ -5,23 +5,30 @@ local ui_get = ui.get
 local FakelagLimit = ui.reference('AA', 'Fake lag', 'Limit')
 local x, y = client.screen_size()
 
---[[
-	x = width
-	y = height
-]]
+local Indicator = {
+	Label = ui.new_label('AA', 'Fake lag', 'Indicator'),
+	LineWidth = ui.new_slider('AA', 'Fake lag', 'Line width', 1, 3, 1, false, nil, 100),
+	LineColor = ui.new_color_picker('AA', 'Fake lag', 'Line color', 255, 255, 255, 255),
+	DisplayFakelagValue = ui.new_checkbox('AA', 'Fake lag', 'Display fakelag value'),
+	FakelagValueColor = ui.new_color_picker('AA', 'Fake lag', 'Fakelag value color', 255, 255, 255, 255),
+	CircleColorLabel = ui.new_label('AA', 'Fake lag', 'Circle color'),
+	CircleColor = ui.new_color_picker('AA', 'Fake lag', 'Circle color', 255, 255, 255, 255),
+}
 
 local SCREEN_MIDDLE = x / 2
 local SCREEN_BOTTOM = y - 15
 
-local CIRCLE_POS = SCREEN_MIDDLE + 0
+local LINE_WIDTH = 100
+
+local CIRCLE_POS = SCREEN_MIDDLE + (-LINE_WIDTH+10)
 local CIRCLE_RADIUS = 10
 
-local FAKELAG_LIMIT
+local FAKELAG_LIMIT = 6
 local chokedCommands, chokedCommandsToRender = 0, 0
 
 local function getCirclePos()
 	if chokedCommandsToRender == 0 then
-		return -90
+		return (-LINE_WIDTH+10)
 	end
 
 	if entity_get_prop(entity_get_game_rules(), 'm_bIsValveDS') == 1 then
@@ -32,16 +39,47 @@ local function getCirclePos()
 
 	local FAKELAG_LIMIT_IN_HALF = FAKELAG_LIMIT / 2
 
-	local INTIAL_POSITION = 90 / FAKELAG_LIMIT_IN_HALF
+	local INTIAL_POSITION = (LINE_WIDTH-10) / FAKELAG_LIMIT_IN_HALF
 	
 	local POSITION_TO_RENDER = chokedCommandsToRender * INTIAL_POSITION
 	
 	if chokedCommandsToRender <= FAKELAG_LIMIT_IN_HALF then
-		return -90 + POSITION_TO_RENDER
+		return (-LINE_WIDTH+10) + POSITION_TO_RENDER
 	else
-		return POSITION_TO_RENDER - 90
+		return POSITION_TO_RENDER - (LINE_WIDTH-10)
 	end
 end
+
+ui.set(Indicator.DisplayFakelagValue, true)
+
+ui.set_callback(Indicator.LineWidth, function (itemNumber)
+	LINE_WIDTH = ui_get(itemNumber) * 100
+	CIRCLE_POS = SCREEN_MIDDLE + getCirclePos()
+end)
+
+local LINE_COLOR = {255, 255, 255, 255}
+ui.set_callback(Indicator.LineColor, function (itemNumber)
+	local lineColor = { ui_get(itemNumber) }
+	for i=1, 4 do
+		LINE_COLOR[i] = lineColor[i]
+	end
+end)
+
+local FAKELAG_VALUE_COLOR = {255, 255, 255, 255}
+ui.set_callback(Indicator.FakelagValueColor, function (itemNumber)
+	local fakelagValueColor = { ui_get(itemNumber) }
+	for i=1, 4 do
+		FAKELAG_VALUE_COLOR[i] = fakelagValueColor[i]
+	end
+end)
+
+local CIRCLE_COLOR = {255, 255, 255, 255}
+ui.set_callback(Indicator.CircleColor, function (itemNumber)
+	local circleColor = { ui_get(itemNumber) }
+	for i=1, 4 do
+		CIRCLE_COLOR[i] = circleColor[i]
+	end
+end)
 
 client.set_event_callback('setup_command', function (e)
 	local getChokedCommands = e.chokedcommands
@@ -53,9 +91,12 @@ client.set_event_callback('setup_command', function (e)
 end)
 
 client.set_event_callback('paint', function ()
-	renderer_line(SCREEN_MIDDLE-100, SCREEN_BOTTOM, CIRCLE_POS-CIRCLE_RADIUS, SCREEN_BOTTOM, 255, 255, 255, 255) -- Line 1 before circle
-	renderer_line(CIRCLE_POS+CIRCLE_RADIUS, SCREEN_BOTTOM, SCREEN_MIDDLE+100, SCREEN_BOTTOM, 255, 255, 255, 255) -- Line 2 after circle
+	renderer_line(SCREEN_MIDDLE-LINE_WIDTH, SCREEN_BOTTOM, CIRCLE_POS-CIRCLE_RADIUS, SCREEN_BOTTOM, LINE_COLOR[1], LINE_COLOR[2], LINE_COLOR[3], LINE_COLOR[4]) -- Line 1 before circle
+	renderer_line(CIRCLE_POS+CIRCLE_RADIUS, SCREEN_BOTTOM, SCREEN_MIDDLE+LINE_WIDTH, SCREEN_BOTTOM, LINE_COLOR[1], LINE_COLOR[2], LINE_COLOR[3], LINE_COLOR[4]) -- Line 2 after circle
 
-	renderer_circle_outline(CIRCLE_POS, SCREEN_BOTTOM, 255, 255, 255, 255, CIRCLE_RADIUS, 0, 1.0, 1)
-	renderer_text(CIRCLE_POS, SCREEN_BOTTOM, 255, 255, 255, 255, 'cb', 0, chokedCommandsToRender)
+	renderer_circle_outline(CIRCLE_POS, SCREEN_BOTTOM, CIRCLE_COLOR[1], CIRCLE_COLOR[2], CIRCLE_COLOR[3], CIRCLE_COLOR[4], CIRCLE_RADIUS, 0, 1.0, 1)
+
+	if ui_get(Indicator.DisplayFakelagValue) then
+		renderer_text(CIRCLE_POS, SCREEN_BOTTOM, FAKELAG_VALUE_COLOR[1], FAKELAG_VALUE_COLOR[2], FAKELAG_VALUE_COLOR[3], FAKELAG_VALUE_COLOR[4], 'cb', 0, chokedCommandsToRender)
+	end
 end)
