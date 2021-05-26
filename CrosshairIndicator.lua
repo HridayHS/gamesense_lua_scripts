02d6a109d50eae30393b6c3ba1db18dd1237ce07
Hriday Sharma <hridaysharma42@gmail.com>
2021-05-26 13:53:08 +0530

    CrosshairIndicator: Update

--- CrosshairIndicator.lua
+++ CrosshairIndicator.lua
@@ -1,26 +1,12 @@
 local renderer_text = renderer.text
 local table_insert = table.insert
 
-local indicators = {}
-
 local x, y = client.screen_size()
 
-local Width = x / 2
-local Height = (y / 2)
+local Width = (x / 2)+6
+local Height = (y / 2)-6
 
-client.set_event_callback('paint', function ()
-	for i=1, #indicators do
-		local indicator = indicators[i]
-
-		local text = indicator.text
-		local r, g, b, a = indicator.r, indicator.g, indicator.b, indicator.a
-
-		renderer_text(Width, Height+(i*15), r, g, b, a, 'c', 0, text)
-
-		-- Remove so we can set again
-		indicators[i] = nil
-	end
-end)
+local indicators = {}
 
 local function IndicatorCallback(indicator)
 	table_insert(indicators, indicator)
@@ -30,4 +16,18 @@ client.set_event_callback('indicator', IndicatorCallback)
 
 client.set_event_callback('shutdown', function ()
 	client.unset_event_callback('indicator', IndicatorCallback)
+end)
+
+-- Main
+client.set_event_callback('paint', function ()
+	for i=1, #indicators do
+		local indicator = indicators[i]
+
+		local text = indicator.text
+		local r, g, b, a = indicator.r, indicator.g, indicator.b, indicator.a
+
+		renderer_text(Width, Height+(i*12), r, g, b, a, nil, 0, text)
+
+		indicators[i] = nil
+	end
 end)
