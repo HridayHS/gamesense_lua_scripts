827e89bd3432030995433612119577330fac896b
Hriday Sharma <hridaysharma42@gmail.com>
2021-05-27 10:38:42 +0530

    CrosshairIndicator: Add DPI support & code improvements

--- CrosshairIndicator.lua
+++ CrosshairIndicator.lua
@@ -1,6 +1,8 @@
-local globals_curtime = globals.curtime
-local renderer_circle_outline, renderer_measure_text, renderer_text = renderer.circle_outline, renderer.measure_text, renderer.text
+local curtime = globals.curtime
+local unset_event_callback = client.unset_event_callback
+local render_circle_outline, measure_text, render_text = renderer.circle_outline, renderer.measure_text, renderer.text
 local table_insert = table.insert
+local ui_get = ui.get
 
 local x, y = client.screen_size()
 
@@ -9,16 +11,27 @@ local Height = (y / 2)+6
 
 local indicators = {}
 
--- Main
 local TIME_TO_PLANT_BOMB = 3
 local timeAtBombWillBePlanted
 
 local function innerCircleOutlinePercentage()
-    local timeElapsed = globals_curtime() + TIME_TO_PLANT_BOMB - timeAtBombWillBePlanted
-    local timeElapsedInPercentage = (timeElapsed / TIME_TO_PLANT_BOMB * 100) + 0.5
-    return timeElapsedInPercentage * 0.01
+    local timeElapsed = (curtime() + TIME_TO_PLANT_BOMB) - timeAtBombWillBePlanted
+    local timeElapsedInPerc = (timeElapsed / TIME_TO_PLANT_BOMB * 100) + 0.5
+    return timeElapsedInPerc * 0.01
 end
 
+-- Gap between indicators text
+local indicatorTextGap = 12
+
+-- Outer circle
+local o_circleRadius = 6
+local o_cricleThickness = o_circleRadius/2
+
+-- Inner circle
+local i_circleRadius = o_circleRadius-1
+local i_cricleThickness = (o_circleRadius-1)/3
+
+-- Main
 client.set_event_callback('paint', function ()
 	for i=1, #indicators do
 		local indicator = indicators[i]
@@ -26,28 +39,27 @@ client.set_event_callback('paint', function ()
 		local text = indicator.text
 		local r, g, b, a = indicator.r, indicator.g, indicator.b, indicator.a
 
-		local textH = Height + (i*-12) + (#indicators*12)
+		local textH = Height + (i*-indicatorTextGap) + (#indicators*indicatorTextGap)
 
-		renderer_text(Width, textH, r, g, b, a, nil, 0, text)
+		render_text(Width, textH, r, g, b, a, 'd', 0, text)
 
 		if isBombBeingPlanted and text:find('Bombsite') then
-			local m_textW, m_textH = renderer_measure_text(nil, text)
+			local m_textW, m_textH = measure_text('d', text)
 
-			local cricleW = Width+m_textW+10
-			local cricleH = textH+(m_textH/1.55)
+			local cricleW = Width+m_textW+o_circleRadius+4
+			local cricleH = textH+(m_textH/1.71)
 
-			renderer_circle_outline(cricleW, cricleH, 0, 0, 0, 200, 6, 0, 1.0, 3)
-			renderer_circle_outline(cricleW, cricleH, 255, 255, 255, 200, 5, 0, innerCircleOutlinePercentage(), 1.6)
+			render_circle_outline(cricleW, cricleH, 0, 0, 0, 200, o_circleRadius, 0, 1.0, o_cricleThickness)
+			render_circle_outline(cricleW, cricleH, 255, 255, 255, 200, i_circleRadius, 0, innerCircleOutlinePercentage(), i_cricleThickness)
 		end
 	end
 
-	for i=1, #indicators do
-		indicators[i] = nil
-	end
+	-- Reset indicator table
+	indicators = {}
 end)
 
 client.set_event_callback('bomb_beginplant', function ()
-    timeAtBombWillBePlanted = globals_curtime() + TIME_TO_PLANT_BOMB
+    timeAtBombWillBePlanted = curtime() + TIME_TO_PLANT_BOMB
     isBombBeingPlanted = true
 end)
 
@@ -67,5 +79,33 @@ end
 client.set_event_callback('indicator', IndicatorCallback)
 
 client.set_event_callback('shutdown', function ()
-	client.unset_event_callback('indicator', IndicatorCallback)
+	unset_event_callback('indicator', IndicatorCallback)
 end)
+
+-- DPI Support
+local DPI_SCALE = ui.reference('Misc', 'Settings', 'DPI scale')
+
+local DPI_SCALE_SETTINGS = {
+	['100%'] = { indicatorTextGap = 12, o_circleRadius = 6 },
+	['125%'] = { indicatorTextGap = 13, o_circleRadius = 7 },
+	['150%'] = { indicatorTextGap = 15, o_circleRadius = 8 },
+	['175%'] = { indicatorTextGap = 18, o_circleRadius = 9 },
+	['200%'] = { indicatorTextGap = 20, o_circleRadius = 10 }
+}
+
+local function DPI_SCALE_HANDLER()
+	local DPI = ui_get(DPI_SCALE)
+
+	-- Update local variables --
+	indicatorTextGap = DPI_SCALE_SETTINGS[DPI].indicatorTextGap
+
+	o_circleRadius = DPI_SCALE_SETTINGS[DPI].o_circleRadius
+	o_cricleThickness = o_circleRadius/2
+
+	i_circleRadius = o_circleRadius-1
+	i_cricleThickness = (o_circleRadius-1)/3
+end
+
+ui.set_callback(DPI_SCALE, DPI_SCALE_HANDLER)
+
+DPI_SCALE_HANDLER()
