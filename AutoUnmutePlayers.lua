07971880f81d38962de632dd393fc8420eb7d729
Hriday Sharma <hridaysharma42@gmail.com>
2022-02-13 21:38:19 +0530

    AutoUnmutePlayers: Fix

--- AutoUnmutePlayers.lua
+++ AutoUnmutePlayers.lua
@@ -1,3 +1,4 @@
+local userid_to_entindex = client.userid_to_entindex
 local entity_get_local_player, entity_get_player_name = entity.get_local_player, entity.get_player_name
 local globals_maxplayers = globals.maxplayers
 
@@ -5,17 +6,18 @@ local GameStateAPI = panorama.open().GameStateAPI
 
 local function UnmutePlayer(player_ent_index)
     local player_xuid = GameStateAPI.GetPlayerXuidStringFromEntIndex(player_ent_index)
-    if player_xuid ~= '0' and GameStateAPI.HasCommunicationAbuseMute(player_xuid) then
+    if player_xuid ~= '0' and GameStateAPI.IsSelectedPlayerMuted(player_xuid) then
         GameStateAPI.ToggleMute(player_xuid)
         print('Unmuted player: ', entity_get_player_name(player_ent_index))
     end
 end
 
 client.set_event_callback('player_connect_full', function (event)
-    local player_ent_index = event.index + 1
+    local playerUserID = event.userid
+    local playerEntIndex = userid_to_entindex(playerUserID)
 
     -- Run through all players, unmute them if muted and then return.
-    if (player_ent_index == entity_get_local_player()) then
+    if (playerEntIndex == entity_get_local_player()) then
         for player=1, globals_maxplayers() do
             UnmutePlayer(player)
         end
